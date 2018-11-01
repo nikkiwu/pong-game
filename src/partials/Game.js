b976e4ff2ff659d3bbf056a22fd160d37326e2bd
Nikki Wu <nikkiwu9@gmail.com>
2018-11-01 13:55:15 -0700

    added second paddle

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -15,8 +15,7 @@ export default class Game {
 		this.boardGap = 10;
 
 
-		this.player1 = new Paddle(
-		    this.height,
+		this.player1 = new Paddle( this.height,
             this.paddleWidth,
             this.paddleHeight,
             this.boardGap,
@@ -30,24 +29,25 @@ export default class Game {
             this.height,
             this.paddleWidth,
             this.paddleHeight,
+            (this.width - this.boardGap - this.paddleWidth),
             ((this.height - this.paddleHeight) / 2),
             KEYS.up,
             KEYS.down
         );
 	
-		// Other code goes here...
 		this.gameElement = document.getElementById(this.element);
         this.board = new Board(this.width, this.height);
 
     } // End of Constructor
 
 	render() {
-		// More code goes here...
         this.gameElement.innerHTML = '';
         let svg = document.createElementNS(SVG_NS, 'svg');
         svg.setAttributeNS(null, 'width', this.width);
         svg.setAttributeNS(null, 'height', this.height);
         svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+
+        //Render SVG
         this.gameElement.appendChild(svg);
         this.board.render(svg);
         this.player1.render(svg);
